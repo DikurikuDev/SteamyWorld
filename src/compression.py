6cacd7c64b91c604c97f24ddf58a12fa9bf2745a
Dikuriku <dikuriku.dev@gmail.com>
2023-05-05 08:33:30 +0100

    add compression

--- src/compression.py
+++ src/compression.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from glob import glob
-from os import remove
-from os.path import basename, dirname, join
+from os import listdir, remove
+from os.path import basename, dirname, getsize, join
 from zipfile import ZipFile
 import zlib
 from logger import Logger
@@ -22,14 +22,71 @@ class Compressor(CompressionHandler):
         self.path = path
         self.logger = options.get("logger", Logger())
 
-    def __impak(self):
-        pass
+    def __impak(self, paths):
+        for path in paths:
+            dir = dirname(path)
+            inpak_path = path[:-5]
+            with open(path, "r") as lock:
+                file_paths = lock.readlines()
+                self.logger.info(f"compressing {inpak_path}...")
+                with ZipFile(inpak_path, "w") as impak_file:
+                    for file_path in file_paths:
+                        relative_path = file_path.strip()
+                        absolute_path = join(dir, relative_path)
+                        self.logger.debug(f"adding {relative_path}...")
+                        impak_file.write(absolute_path, relative_path)
+                        self.logger.debug(f"removing {absolute_path}...")
+                        remove(absolute_path)
+                        self.logger.debug(f"removing folder (if empty)...")
+                        _remove_folder_if_empty(dirname(absolute_path))
 
-    def __z(self):
-        pass
+            self.logger.debug(f"removing {path}...")
+            remove(path)
+
+            self.logger.debug("done!")
+
+    def __z(self, paths):
+        for path in paths:
+            dir = dirname(path)
+            with open(path, "r") as lock:
+                file_paths = lock.readlines()
+                for file_path in file_paths:
+                    original_path = join(dir, file_path.strip())
+                    relative_path = f"{file_path.strip()}.z"
+                    absolute_path = join(dir, relative_path)
+
+                    self.logger.info(f"compressing {absolute_path}...")
+                    with open(absolute_path, "wb") as zfile:
+                        # adding header        
+                        file_size = _get_file_size(original_path)
+                        file_size_as_bytes = file_size.to_bytes(4, "little")
+                        zfile.write(file_size_as_bytes)
+                        with open(original_path, "rb") as file:
+                            # adding file
+                            uncompressed_data = file.read()
+                            compressed_data = zlib.compress(uncompressed_data)            
+                            zfile.write(compressed_data)
+                    
+                    self.logger.debug(f"removing {original_path}...")
+                    remove(original_path)
+            
+            self.logger.debug(f"removing {file_path}...")
+            remove(path)
+
+            self.logger.debug("done!")
 
     def start(self):
-        pass
+        self.logger.info(f"at folder {self.path}:")
+        self.logger.info("starting compression...")
+        self.logger.debug("searching .z.lock files...")
+        paths = _find_all_files(self.path, "z.lock")
+        self.logger.debug(f"found {len(paths)} file(s)!")
+        self.logger.debug("searching .impak.lock files...")
+        self.__z(paths)
+        paths = _find_all_files(self.path, "*.impak.lock")
+        self.logger.debug(f"found {len(paths)} file(s)!")
+        self.__impak(paths)
+        self.logger.info("compression done!")
 
 
 class Decompressor(CompressionHandler):
@@ -85,15 +142,22 @@ class Decompressor(CompressionHandler):
         self.logger.info(f"at folder {self.path}:")
         self.logger.info("starting decompression...")
         self.logger.debug("searching .z files...")
-        paths = _find_all_files_with_extension(self.path, "z")
+        paths = _find_all_files(self.path, "*.z")
         self.logger.debug(f"found {len(paths)} file(s)!")
         self.logger.debug("searching .impak files...")
         self.__z(paths)
-        paths = _find_all_files_with_extension(self.path, "impak")
+        paths = _find_all_files(self.path, "*.impak")
         self.logger.debug(f"found {len(paths)} file(s)!")
         self.__impak(paths)
         self.logger.info("decompression done!")
 
 
-def _find_all_files_with_extension(path, extension):
-    return glob(f"{path}/**/*.{extension}", recursive=True)
+def _find_all_files(path, filename_pattern):
+    return glob(f"{path}/**/{filename_pattern}", recursive=True)
+
+def _get_file_size(path):
+    return getsize(path)
+
+def _remove_folder_if_empty(path):
+    if not listdir(path):
+        remove(path)
